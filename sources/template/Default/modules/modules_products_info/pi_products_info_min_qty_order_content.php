421399ab22b971b62a2b4a144c9d1025fc11d982
ClicShopping <admin@clicshopping.org>
2021-10-30 11:50:51 +0200

    update

--- sources/template/Default/modules/modules_products_info/pi_products_info_min_qty_order_content.php
+++ sources/template/Default/modules/modules_products_info/pi_products_info_min_qty_order_content.php
@@ -14,10 +14,10 @@
   use ClicShopping\OM\CLICSHOPPING;
 
   class pi_products_info_min_qty_order_content {
-    public $code;
+    public string $code;
     public $group;
-    public string $title;
-    public string $description;
+    public $title;
+    public $description;
     public ?int $sort_order = 0;
     public bool $enabled = false;
 
